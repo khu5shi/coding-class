b96f0c8bc0bfe26a4de0afdb940c70eb922b474c
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-02 12:09:11 +0530

    Create pattern9.c

---
+++ patterns/pattern9.c
@@ -0,0 +1,21 @@
+*\
+1
+22
+333
+4444
+55555
+\*
+#include <stdio.h>
+int main()
+{int i,a,b;
+  for(i=1;i<=5;i++)
+    { for(a=1;a<=i;a++)
+    {printf("%d",i);
+
+
+    }
+    printf("\n");
+
+  }
+  return 0;
+  }
