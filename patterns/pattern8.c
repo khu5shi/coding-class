4ad0e290698a829a4c34d4236541955c5eca442a
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-02 12:12:22 +0530

    Create pattern8.c

---
+++ patterns/pattern8.c
@@ -0,0 +1,21 @@
+*\
+1
+12
+123
+1234
+12345
+\*
+#include <stdio.h>
+int main()
+{int i,a,b;
+  for(i=1;i<=5;i++)
+    { for(a=1;a<=i;a++)
+    {printf("%d",a);
+
+
+    }
+    printf("\n");
+
+  }
+  return 0;
+  }
