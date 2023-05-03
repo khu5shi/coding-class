eb831ed2eb75f77805673576b69e72f27da0c809
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-04 02:25:19 +0530

    Create pattern16.c

---
+++ patterns/pattern16.c
@@ -0,0 +1,26 @@
+*\
+55555
+4444
+333
+22
+1
+\*
+#include<stdio.h>
+int main()
+{
+    int i,a;
+     i=5;
+    while(i>=1)
+    {   a=1;
+        while(a<=i)
+     {
+        printf("%d",i);
+        a++;
+
+     }
+     printf("\n");
+     i--;
+     }
+     return 0;
+
+}
