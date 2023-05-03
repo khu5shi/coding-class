b5350aa0fbdb0b120b22f4427ddaff58de21fc18
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-04 01:24:20 +0530

    Update pattern-1.c

--- patterns/pattern-1.c
+++ patterns/pattern-1.c
@@ -1,1 +1,22 @@
+*\
+*****
+****
+***
+**
+*
+\*
+#include <stdio.h>
+int main()
+{int i,a;
+  for(i=5;i>=1;i--)
+    { for(a=1;a<=i;a++)
+    {printf("*");
+
+
+    }
+    printf("\n");
+
+  }
+  return 0;
+  }
 
