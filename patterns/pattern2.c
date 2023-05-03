0224670775e00de76fd61a9a424cde7fd50c805f
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-03 12:15:03 +0530

    Create pattern2.c

---
+++ patterns/pattern2.c
@@ -0,0 +1,25 @@
+*\
+1
+23
+456
+\*
+#include <stdio.h>
+int main()
+{int i,a,b=1;
+  for(i=1;i<=3;i++)
+    {
+      
+         for(a=1;a<=i;a++)
+    
+     {
+        printf("%d",b);
+        b++;
+        }
+     
+
+    
+    printf("\n");
+
+  }
+  return 0;
+  }
