fb348046cf17f3ddbe410e39033bc7e09b5f3915
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-04 02:22:57 +0530

    Create pattern15.c

---
+++ patterns/pattern15.c
@@ -0,0 +1,25 @@
+*\
+1
+22
+333
+4444
+55555
+\*
+#include<stdio.h>
+int main()
+{int i,a;
+ i=1;
+ while(i<=5)
+ {
+    a=1;
+    while(a<=i)
+    {
+        printf("%d",i);
+        a++;
+    }
+    printf("\n");
+    i++;
+
+ }
+ return 0;
+ }
