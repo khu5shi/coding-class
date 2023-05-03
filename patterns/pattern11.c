e45b8fe344066ffdaf644f17ffdc3fac951235a9
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-04 02:04:04 +0530

    Create pattern11.c

---
+++ patterns/pattern11.c
@@ -0,0 +1,25 @@
+*\
+*
+**
+***
+****
+*****
+\*
+#include<stdio.h>
+int main()
+{int i,a;
+ i=1;
+ while(i<=5)
+ {
+    a=1;
+    while(a<=i)
+    {
+        printf("*");
+        a++;
+    }
+    printf("\n");
+    i++;
+
+ }
+ return 0;
+ }
