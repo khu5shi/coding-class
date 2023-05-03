81a988b95a6849d82d2108ffbb889ee634c99e14
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-03 11:50:00 +0530

    Create pattern4.c

---
+++ patterns/pattern4.c
@@ -0,0 +1,21 @@
+*\
+55555
+4444
+333
+22
+1
+\*
+#include <stdio.h>
+int main()
+{int i,a;
+  for(i=5;i>=1;i--)
+    { for(a=1;a<=i;a++)
+    {printf("%d",i);
+
+
+    }
+    printf("\n");
+
+  }
+  return 0;
+  }
