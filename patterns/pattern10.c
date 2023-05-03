85b0c27d2fba8105e89404d5840e55b9fd10d444
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-04 01:59:23 +0530

    Create pattern10.c

---
+++ patterns/pattern10.c
@@ -0,0 +1,26 @@
+*\
+*****
+****
+***
+**
+*
+\*
+#include<stdio.h>
+int main()
+{
+    int i,a;
+     i=5;
+    while(i>=1)
+    {   a=1;
+        while(a<=i)
+     {
+        printf("*");
+        a++;
+
+     }
+     printf("\n");
+     i--;
+     }
+     return 0;
+
+}
